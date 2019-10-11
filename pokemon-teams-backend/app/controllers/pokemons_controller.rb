78ecb5c422f84d454476d36cf9187f785f9337fb
tan-nate <nktan93@gmail.com>
2019-10-11 11:12:02 -0700

    Done.

--- pokemon-teams-backend/app/controllers/pokemons_controller.rb
+++ pokemon-teams-backend/app/controllers/pokemons_controller.rb
@@ -1,2 +1,13 @@
 class PokemonsController < ApplicationController
+    def index
+        pokemons = Pokemon.all
+        render json: PokemonSerializer.new(pokemons).to_serialized_json
+    end
+    
+    def create
+        name = Faker::Name.first_name
+        species = Faker::Games::Pokemon.name
+        Pokemon.create(nickname: name, species: species, trainer_id: params[:trainer_id])
+        render json: PokemonSerializer.new(pokemon).to_serialized_json
+    end
 end
--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,59 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+
+fetch(TRAINERS_URL)
+    .then(resp => resp.json())
+    .then(json => json.forEach(function(trainer) {
+        let trainerCard = document.createElement('div');
+        trainerCard.className = "card";
+        trainerCard.setAttribute("data-id", trainer.id);
+        let p = document.createElement('p');
+        p.innerHTML = trainer.name;
+        let button = document.createElement('button');
+        button.setAttribute("data-trainer-id", trainer.id);
+        button.innerHTML = "Add Pokemon";
+        button.addEventListener('click', addPokemon(trainer.id));
+        let ul = document.createElement('ul');
+        trainer.pokemons.forEach(function(pokemon, index) {
+            let li = document.createElement('li');
+            li.innerText = `${pokemon.nickname} (${pokemon.species})`;
+            let releaseButton = document.createElement('button');
+            releaseButton.className = "release";
+            releaseButton.setAttribute("data-pokemon-id", pokemon.id);
+            releaseButton.innerText = "Release";
+            li.appendChild(releaseButton);
+            ul.appendChild(li);
+        });
+        [p, button, ul].forEach(element => trainerCard.appendChild(element));
+        document.querySelector('main').appendChild(trainerCard);
+    }));
+
+function addPokemon(trainer_id) {
+    formData = {
+        trainer_id: trainer_id
+    };
+}
+
+let formData;
+       
+let configObj = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+};
+       
+fetch(POKEMONS_URL, configObj)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(object) {
+        console.log(object);
+    })
+    .catch(function(error) {
+        alert("Bad things! Ragnarők!");
+        console.log(error.message);
+    });
