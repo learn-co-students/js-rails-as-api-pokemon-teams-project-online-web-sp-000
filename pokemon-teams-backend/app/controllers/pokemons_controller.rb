06a97f6b0a8f0700799313965a3e7309437fa8d5
tan-nate <nktan93@gmail.com>
2019-10-11 14:04:44 -0700

    Done.

--- pokemon-teams-backend/app/controllers/pokemons_controller.rb
+++ pokemon-teams-backend/app/controllers/pokemons_controller.rb
@@ -7,7 +7,13 @@ class PokemonsController < ApplicationController
     def create
         name = Faker::Name.first_name
         species = Faker::Games::Pokemon.name
-        Pokemon.create(nickname: name, species: species, trainer_id: params[:trainer_id])
+        pokemon = Pokemon.create(nickname: name, species: species, trainer_id: params[:trainer_id])
         render json: PokemonSerializer.new(pokemon).to_serialized_json
+    end
+
+    def destroy
+        pokemon = Pokemon.find(params[:pokemon_id])
+        render json: PokemonSerializer.new(pokemon).to_serialized_json
+        pokemon.destroy
     end
 end
--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -13,15 +13,16 @@ fetch(TRAINERS_URL)
         let button = document.createElement('button');
         button.setAttribute("data-trainer-id", trainer.id);
         button.innerHTML = "Add Pokemon";
-        button.addEventListener('click', addPokemon(trainer.id));
+        button.addEventListener('click', addPokemon);
         let ul = document.createElement('ul');
-        trainer.pokemons.forEach(function(pokemon, index) {
+        trainer.pokemons.forEach(function(pokemon) {
             let li = document.createElement('li');
             li.innerText = `${pokemon.nickname} (${pokemon.species})`;
             let releaseButton = document.createElement('button');
             releaseButton.className = "release";
             releaseButton.setAttribute("data-pokemon-id", pokemon.id);
             releaseButton.innerText = "Release";
+            releaseButton.addEventListener('click', deletePokemon);
             li.appendChild(releaseButton);
             ul.appendChild(li);
         });
@@ -29,31 +30,70 @@ fetch(TRAINERS_URL)
         document.querySelector('main').appendChild(trainerCard);
     }));
 
-function addPokemon(trainer_id) {
-    formData = {
-        trainer_id: trainer_id
+function addPokemon() {
+    let trainerId = this.getAttribute('data-trainer-id');
+    let button = this;
+    
+    let formData = {
+        trainer_id: trainerId
+    }
+
+    let configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
     };
+           
+    fetch(POKEMONS_URL, configObj)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(pokemon) {
+            let li = document.createElement('li');
+            li.innerText = `${pokemon.nickname} (${pokemon.species})`;
+            let releaseButton = document.createElement('button');
+            releaseButton.className = "release";
+            releaseButton.setAttribute("data-pokemon-id", pokemon.id);
+            releaseButton.innerText = "Release";
+            releaseButton.addEventListener('click', deletePokemon);
+            li.appendChild(releaseButton);
+            button.parentNode.querySelector('ul').appendChild(li);
+        })
+        .catch(function(error) {
+            alert("Bad things! Ragnarők!");
+            console.log(error.message);
+        });
 }
 
-let formData;
-       
-let configObj = {
-    method: "POST",
-    headers: {
-        "Content-Type": "application/json",
-        "Accept": "application/json"
-    },
-    body: JSON.stringify(formData)
-};
-       
-fetch(POKEMONS_URL, configObj)
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(object) {
-        console.log(object);
-    })
-    .catch(function(error) {
-        alert("Bad things! Ragnarők!");
-        console.log(error.message);
-    });
+function deletePokemon() {
+    let pokemonId = this.getAttribute('data-pokemon-id');
+    let button = this;
+    
+    let formData = {
+        pokemon_id: pokemonId
+    }
+
+    let configObj = {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+    };
+           
+    fetch(`${POKEMONS_URL}/${pokemonId}`, configObj)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function() {
+            button.parentNode.remove();
+        })
+        .catch(function(error) {
+            alert("Bad things! Ragnarők!");
+            console.log(error.message);
+        });
+}
