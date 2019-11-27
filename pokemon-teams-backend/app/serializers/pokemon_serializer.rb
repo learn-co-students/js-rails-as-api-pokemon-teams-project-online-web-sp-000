9704a18a89fafcdf3a807cab605bb2ae4f89fcb3
Adil Naimi <adilnm23@hotmail.com>
2019-11-26 18:33:26 -0600

    Done.

--- pokemon-teams-backend/app/serializers/pokemon_serializer.rb
+++ pokemon-teams-backend/app/serializers/pokemon_serializer.rb
@@ -1,4 +1,4 @@
 class PokemonSerializer
   include FastJsonapi::ObjectSerializer
-  attributes 
+  attributes :nickname, :species, :trainer_id
 end
--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -9,10 +9,30 @@ fetch (TRAINERS_URL).then((res)=>res.json()).then((json)=>{
     const main=document.querySelector('main')
     main.appendChild(div)
     div.className="card"
+    div.setAttribute("data-id",item.id)
     div.innerHTML=`<p>${item.attributes.name}</p>`
     const btn=document.createElement('button')
     div.appendChild(btn)
     btn.innerHTML="Add Pokemon"
+    btn.setAttribute("data-trainer-id",item.id)
+    btn.addEventListener('click',(e)=>{
+    let trainerId=div.getAttribute("data-id")
+
+      let configObj = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify({trainer_id: trainerId})
+      }
+
+      fetch(POKEMONS_URL, configObj).then((res)=>(res.json()))
+
+      location.reload();
+    })
+
+
     const ul=document.createElement('ul')
     div.appendChild(ul)
       item.attributes.pokemons.forEach(function (i) {
@@ -23,6 +43,18 @@ fetch (TRAINERS_URL).then((res)=>res.json()).then((json)=>{
         li.appendChild(release_btn)
         release_btn.className="release"
         release_btn.innerHTML="Release"
+        release_btn.setAttribute("data-pokemon-id",i.id)
+        release_btn.addEventListener('click',(e)=> {
+          pokemon_id=e.target.getAttribute("data-pokemon-id")
+          let configObj = {
+            method: "delete"
+          }
+
+          fetch(POKEMONS_URL+`/${pokemon_id}`, configObj).then((res)=>(res.json()))
+
+        })
+
       })
   })
+
 })
