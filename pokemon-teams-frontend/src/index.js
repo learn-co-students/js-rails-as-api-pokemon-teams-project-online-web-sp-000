8033098f0121929d723c0804176eeebd28e0b887
Kristinamarih <kristinahorensky@gmail.com>
2019-12-08 15:21:25 -0800

    Done.

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -2,3 +2,12 @@ const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
 
+getTrainers() {
+    return fetch(TRAINERS_URL).then(res => res.json()
+    )
+}
+
+getPokemons() {
+    return fetch(POKEMONS_URL).then(res => res.json()
+    )
+}
