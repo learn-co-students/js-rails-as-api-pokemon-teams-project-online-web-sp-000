8668c202b3e2f99c9b6b96195e687a2dd4a28be3
Kristinamarih <kristinahorensky@gmail.com>
2019-12-08 15:53:13 -0800

    Done.

--- pokemon-teams-backend/app/controllers/pokemons_controller.rb
+++ pokemon-teams-backend/app/controllers/pokemons_controller.rb
@@ -1,7 +1,13 @@
 class PokemonsController < ApplicationController
 
     def new
-
+        pokemon = Pokemon.new
+        pokemon.nickname = Faker::Name.nickname
+        pokemon.species = Faker::Games::Pokemon.species
+        if trainer.pokemon.count < 6
+            pokemon.save
+        end
+        render json: pokemons, include: [:nickname, :species, :trainer_id]
     end
 
     def delete
--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,13 +1,46 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+const mainDiv = document.querySelector('main')
 
-getTrainers() {
-    return fetch(TRAINERS_URL).then(res => res.json()
-    )
+fetch(TRAINERS_URL)
+    .then(res => res.json())
+    .then(trainersToPage)
+
+function trainersToPage(trainers) {
+    trainers.forEach(trainer => {
+        let pokemonString = ""
+        trainer.pokemons.forEach(pokemon => {
+            pokemonString += `<li>${pokemon.nickname} (${pokemon.species}) <button class="release" data-pokemon-id="${pokemon.id}">Release</button></li>`
+        })
+        mainDiv.innerHTML += `
+        <div class="card" data-id="1"><p>${trainer.name}</p>
+        <button data-trainer-id="${trainer.id}">Add Pokemon</button>
+        <ul>${pokemonString}</ul>
+        </div>
+        `
+    })
+    mainDiv.addEventListener('click', e => {
+        if (e.target.dataset.trainerId !== undefined) {
+            fetch(POKEMONS_URL, {
+                method: "POST",
+                headers: {
+                    'Content Type': 'application/json'
+                },
+                body: JSON.stringify( {
+                    trainer_id: e.target.dataset.trainerId
+                })
+            })
+            .then(res => res.json())
+            .then(addPokemon)
+        }
+        if (e.target.dataset.pokemonId !== undefined) {
+            e.target.parentElement.remove()
+            fetch(POKEMONS_URL + '/' + e.target.dataset.pokemonId, {method : "DELETE"})
+        }
+    })
 }
 
-getPokemons() {
-    return fetch(POKEMONS_URL).then(res => res.json()
-    )
+function addPokemon(pokemon) {
+    mainDiv.children[pokemon.trainer_id-1].lastElementChild.innerHTML += `<li>${pokemon.nickname} (${pokemon.species}) <button class="release" data-pokemon-id="${pokemon.id}">Release</button></li>`
 }
