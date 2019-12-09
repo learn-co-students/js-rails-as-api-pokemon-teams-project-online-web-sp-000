5ac2afa3ea7fc5283a8367998e967aa4b461dd1d
Mark Delgado <markpnoi26@gmail.com>
2019-12-09 06:13:29 -0800

    backend edited

--- pokemon-teams-backend/app/controllers/pokemons_controller.rb
+++ pokemon-teams-backend/app/controllers/pokemons_controller.rb
@@ -1,2 +1,11 @@
 class PokemonsController < ApplicationController
+  def index
+    pokemons = Pokemon.all
+    render json: pokemons, except: [:created_at, :updated_at]
+  end
+
+  def show
+    pokemon = Pokemon.find_by(id: params[:id])
+    render json: pokemon, except: [:created_at, :updated_at]
+  end
 end
--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,21 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+
+// <div class="card" data-id="1"><p>Prince (Reference Only)</p>
+//   <button data-trainer-id="1">Add Pokemon</button>
+//   <ul>
+//     <li>Jacey (Kakuna) <button class="release" data-pokemon-id="140">Release</button></li>
+//     <li>Zachariah (Ditto) <button class="release" data-pokemon-id="141">Release</button></li>
+//     <li>Mittie (Farfetch'd) <button class="release" data-pokemon-id="149">Release</button></li>
+//     <li>Rosetta (Eevee) <button class="release" data-pokemon-id="150">Release</button></li>
+//     <li>Rod (Beedrill) <button class="release" data-pokemon-id="151">Release</button></li>
+//   </ul>
+// </div>
+
+// sample card for each trainer
+
+document.addEventListener('DOMContentLoaded', () =>{
+  // load content here
+  
+})
