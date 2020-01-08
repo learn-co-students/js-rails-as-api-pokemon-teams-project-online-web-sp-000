04f8aa837b084aeaea7c181ded4b3ba0e460c6e4
jgrantps <jgrantps@hotmail.com>
2020-01-08 13:44:45 -0400

    Done.

---
+++ pokemon-teams-backend/app/controllers/trainers_controller.rb
@@ -0,0 +1,13 @@
+class TrainersController < ApplicationController
+
+  def index
+  trainers = Trainer.all
+  options = {
+    include: [:pokemons]
+  }
+  render json: TrainerSerializer.new(trainers)
+  
+  end
+
+  
+end
