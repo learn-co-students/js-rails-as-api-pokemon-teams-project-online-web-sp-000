3096faf120f8e7c223b3d47c9024c174e80ad1a7
Lindsey Wells <github email address>
2020-03-28 09:53:10 +0000

    Done.

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,45 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+
+
+function getTrainers() {
+    fetch(TRAINERS_URL)
+        .then(response => response.json())
+        .then(trainers => createTrainers(trainers))
+}
+
+function createTrainers(trainers) {
+    main = document.querySelector('main')
+    for (const trainer of trainers) {
+        card = document.createElement('div')
+        card.classList.add("card")
+
+        p = document.createElement('p')
+        p.innerText = trainer.name
+
+        button = document.createElement('button')
+        button.innerText = "Add Pokemon"
+
+        ul = document.createElement('ul')
+        for (const pokemon of trainer.pokemons) {
+            li = document.createElement('li')
+            libutton = document.createElement('button')
+            libutton.innerText = "Release"
+            libutton.classList.add("release")
+            li.innerText = `${pokemon.species} (${pokemon.nickname})`
+
+            li.appendChild(libutton)
+            ul.appendChild(li)
+        }
+
+
+        card.appendChild(p)
+        card.appendChild(button)
+        card.appendChild(ul)
+
+        main.appendChild(card)
+    }
+}
+
+getTrainers()
