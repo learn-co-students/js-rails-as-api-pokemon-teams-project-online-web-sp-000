11e0a2d011ad746d5cff46cf0cf02367b2c67185
Lindsey Wells <github email address>
2020-03-28 20:35:26 +0000

    Done.

--- pokemon-teams-backend/app/controllers/pokemons_controller.rb
+++ pokemon-teams-backend/app/controllers/pokemons_controller.rb
@@ -1,2 +1,9 @@
 class PokemonsController < ApplicationController
+  def create
+    trainer = Trainer.find(params[:id])
+    pokemon_name = Faker::Name.first_name
+    species = Faker::Games::Pokemon.name
+    trainer.pokemons.build(:nickname => pokemon_name, :species => species)
+    render json: TrainerSerializer.new(trainer).to_serialized_json
+  end
 end
--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -12,14 +12,44 @@ function getTrainers() {
 function createTrainers(trainers) {
     main = document.querySelector('main')
     for (const trainer of trainers) {
+        makeTrainerCard(trainer)
+    }
+
+    function addPokemon(trainer) {
+        alert("I was clicked!");
+
+        configObj = {
+            method: "POST",
+            headers: {
+                "Content-Type": 'application/json',
+            },
+            body: JSON.stringify(trainer)
+        }
+
+        fetch(POKEMONS_URL, configObj)
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(object) {
+                card = document.querySelector('main')
+                console.log(object);
+            });
+    }
+
+    function makeTrainerCard(trainer) {
         card = document.createElement('div')
         card.classList.add("card")
+        card.setAttribute('data-id', trainer.id)
 
         p = document.createElement('p')
         p.innerText = trainer.name
 
         button = document.createElement('button')
         button.innerText = "Add Pokemon"
+        button.setAttribute('data-trainer-id', trainer.id)
+        button.addEventListener('click', function(event) {
+            addPokemon(trainer)
+        })
 
         ul = document.createElement('ul')
         for (const pokemon of trainer.pokemons) {
@@ -40,6 +70,7 @@ function createTrainers(trainers) {
 
         main.appendChild(card)
     }
+
 }
 
 getTrainers()
