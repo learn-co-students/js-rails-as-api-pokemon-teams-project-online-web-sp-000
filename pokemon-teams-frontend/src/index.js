62e6b0a5310cb57b7f78e284f0c703112a34ca8f
Soner Mezgitci <you@example.com>
2020-04-01 19:47:46 -0400

    Done.

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -57,80 +57,60 @@ document.addEventListener("DOMContentLoaded", ()=>{
             newPoke = document.createElement("li")
             newPoke.innerHTML = newPokemon.nickname
             newPoke.innerHTML+= `<button class="release" data-pokemon-id="${newPokemon.id}">Release</button>`
+            newPoke.addEventListener("click", releaseButton)
             pokemonList.append(newPoke)
 
 
 
         }) 
        
-        })
+    }) 
     
+            function releaseButton(event){
+                event.preventDefault()
+            debugger
+            }
+        //    pokemonList = document.getElementById(`pokemon-list-${trainer.id}`)
             
-           pokemonList = document.getElementById(`pokemon-list-${trainer.id}`)
-            
-           trainer.pokemons.forEach(pokemon => {
+        //    trainer.pokemons.forEach(pokemon => {
                
                
-        //    pokemonList.innerHTML+= `<li> ${pokemon.nickname}</li>`
-            newPoke = document.createElement("li")
-            button = document.createElement("button") 
-            newPoke.innerHTML = pokemon.nickname
-            button.className = "release"
-            button.innerHTML = "release"
-            button.dataset.pokemonId = pokemon.id
-            // newPoke.innerHTML+= `<button class="release" data-pokemon-id="${pokemon.id}">Release</button>`
-            newPoke.append(button)
+        // //    pokemonList.innerHTML+= `<li> ${pokemon.nickname}</li>`
+        //     newPoke = document.createElement("li")
+        //     button = document.createElement("button") 
+        //     newPoke.innerHTML = pokemon.nickname
+        //     button.className = "release"
+        //     button.innerHTML = "release"
+        //     button.dataset.pokemonId = pokemon.id
+        //     // newPoke.innerHTML+= `<button class="release" data-pokemon-id="${pokemon.id}">Release</button>`
+        //     newPoke.append(button)
             
-            button.addEventListener("click", function(e) {
-                console.log(e.target)
+        //     button.addEventListener("click", function(e) {
+        //         console.log(e.target)
 
-            fetch(`http://localhost:3000/pokemons/${newPoke.id}`, {
-                method:"DELETE",
-            })
+        //     fetch(`http://localhost:3000/pokemons/${newPoke.id}`, {
+        //         method:"DELETE",
+        //     })
     
-               .then(r=> r.json())
-               .then((response) =>{
-                newPoke.remove
-               })
+        //        .then(r=> r.json())
+        //        .then((response) =>{
+        //         newPoke.remove
+        //        })
            
-               })
-               pokemonList.append(newPoke)
-            })
+        //        })
+        //        pokemonList.append(newPoke)
+        //     })
           
             
           
 
         
-        })
-        
-        
-        
-    });
-
-    
-})
-
-
-
-        // releaseButton.addEventListener("click", function (e){
-           
-        //     console.log(e.target.id)
-    
-        //  trainer_id = e.target.id
-
-
-        // //  fetch(`http://localhost:3000/pokemons/${newPokemon.id}`, {
-        // //     method:"DELETE",
-        // // })
-
-        // //    .then(r=> r.json())
-        // //    .then((response) =>{
-        // //     console.log(response)
-        // //    })
-
-        
- 
         // })
+        
+        
+        
+    
+
 
 
 
