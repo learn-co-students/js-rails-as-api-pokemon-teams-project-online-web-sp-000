433fd486ac890efec2d7795362254379532db501
“Oliver <oliverpdahl@gmail.com>
2020-04-11 19:44:52 -0400

    makepokemonli method

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,16 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+
+function makeTrainerCard(){
+
+}
+
+function makePokemonLi(pokemon){
+  let li = document.createElement('li')
+  li.innerText = `${pokemon.nickname} (${pokemon.species})`
+  releaseButton = document.createElement('button')
+  releaseButton.className = 'release'
+  release.innerText = 'Release'
+  li['data-pokemon-id'] = pokemon.id
+}
