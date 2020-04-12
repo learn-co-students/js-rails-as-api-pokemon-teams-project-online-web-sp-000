8dbe6e354ce0c98d1da7c1dc21115703f793f78d
“Oliver <oliverpdahl@gmail.com>
2020-04-11 22:33:50 -0400

    Add post path

--- pokemon-teams-backend/app/controllers/pokemons_controller.rb
+++ pokemon-teams-backend/app/controllers/pokemons_controller.rb
@@ -3,4 +3,11 @@ class PokemonsController < ApplicationController
     pokemon = Pokemon.find(params[:id])
     render json: PokemonSerializer.new(pokemon)
   end
+
+  def create
+    pokemon = Trainer.find(params[trainer_id]).pokemons.build
+    pokemon.name = Faker::Name
+    pokemon.species = Faker::Games::Pokemon
+    pokemon.save
+    render json: PokemonSerializer.new(pokemon)
 end
