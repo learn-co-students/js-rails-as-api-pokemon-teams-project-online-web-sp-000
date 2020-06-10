ff93eba77123274b5a7c807df02e3e127785e36d
lukesherwood <luke.sherwood55@gmail.com>
2020-06-10 21:33:18 +1200

    Done.

---
+++ pokemon-teams-backend/app/controllers/pokemons_controller.rb
@@ -0,0 +1,11 @@
+class PokemonsController < ApplicationController
+    def show
+        pokemon = Pokemon.find(params[:id])
+        render json: PokemonSerializer.new(pokemon)
+    end
+
+    def index
+        pokemons = Pokemon.all
+        render json: PokemonSerializer.new(pokemons)
+    end
+end
