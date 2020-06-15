96d26399f6a623f73113d010fbca123ecef79d28
rehmanali001 <rehmanali001@gmail.com>
2020-06-15 11:40:30 -0500

    Done.

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,129 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+
+
+
+
+
+const pretendPokemon = {id: 2, nickname:'Bob', species:'Squirtal'}
+const pretendTrainer = {id: 3, name: 'Steven', pokemons:[pretendPokemon]}
+
+
+function makeTrainerCard(trainer){
+  let div = document.createElement('div')
+  div.className = 'card'
+  div.id = trainer.attributes.id
+  div['data-id'] = trainer.attributes.id
+  let p = document.createElement('p')
+  p.innerText = trainer.attributes.name
+  div.appendChild(p)
+  let button = document.createElement('button')
+  button['data-trainer-id'] = trainer.attributes.id
+  button.innerText = 'Add Pokemon'
+  div.appendChild(button)
+  addPokemon(button, trainer)
+  let ul = document.createElement('ul')
+  createTrainerPokemon(trainer, ul)
+  div.appendChild(ul)
+  return div
+}
+
+function createTrainerPokemon(trainer, ul){
+  for(const pokemon of trainer.attributes.pokemons){
+    ul.appendChild(makePokemonLi(pokemon))
+  }
+}
+
+function makePokemonLi(pokemon){
+  let li = document.createElement('li')
+  li.innerText = `${pokemon.nickname} (${pokemon.species})`
+  releaseButton = document.createElement('button')
+  releaseButton.className = 'release'
+  releaseButton.innerText = 'Release'
+  li.appendChild(releaseButton)
+  li['data-pokemon-id'] = pokemon.id
+  deletePokemon(releaseButton, li, pokemon)
+  return li
+}
+
+function getTrainers(){
+  fetch(TRAINERS_URL)
+  .then(function(response){
+    return response.json();
+  })
+  .then(function(json){
+    displayTrainers(json.data)
+  })
+  .catch(function(error){
+    console.log(error)
+  })
+}
+
+function displayTrainers(trainers){
+  main = document.querySelector('main')
+  for(const trainer of trainers){
+    main.appendChild(makeTrainerCard(trainer))
+  }
+}
+
+function addPokemon(button, trainer){
+  button.addEventListener('click', function(event) {
+    let formData = {
+      trainer_id: `${trainer.id}`
+    }
+
+    let configObj = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: JSON.stringify(formData)
+    };
+
+    fetch(POKEMONS_URL, configObj)
+      .then(function(response){
+        return response.json();
+      })
+      .then(function(object){
+        let ul = document.getElementById(trainer.id).getElementsByTagName('ul')[0]
+        ul.appendChild(makePokemonLi(object.data.attributes))
+      })
+      .catch(function(error){
+        console.log(error.message);
+      })
+  });
+}
+
+function deletePokemon(button, li, pokemon){
+  button.addEventListener('click', function(event){
+    let formData = {
+      id: pokemon.id,
+      nickname: pokemon.nickname,
+      species: pokemon.species,
+      trainer_id: pokemon.trainer_id
+    }
+
+    let configObj = {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: JSON.stringify(formData)
+    };
+
+    fetch(`${POKEMONS_URL}/${pokemon.id}`, configObj)
+      .then(function(response){
+        li.parentElement.removeChild(li)
+      })
+      .catch(function(error){
+        console.log(error.message);
+      })
+  })
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  getTrainers()
+})
