8f3692eb3f69a6813ffbf1f3480576567f71478d
SPerfors <stevecanbuild@gmail.com>
2020-07-22 13:27:08 -0600

    Done.

--- pokemon-teams-backend/app/controllers/pokemons_controller.rb
+++ pokemon-teams-backend/app/controllers/pokemons_controller.rb
@@ -1,2 +1,13 @@
 class PokemonsController < ApplicationController
+
+    def index 
+        pokemons = Pokemon.all
+        render json: PokemonSerializer.new(pokemons)
+    end
+
+    def show 
+        pokemon = Pokemon.find_by(id: params[:id])
+        render json: PokemonSerializer.new(pokemon)
+    end
+
 end
--- pokemon-teams-backend/app/serializers/pokemon_serializer.rb
+++ pokemon-teams-backend/app/serializers/pokemon_serializer.rb
@@ -1,4 +1,4 @@
 class PokemonSerializer
     include FastJsonapi::ObjectSerializer 
-    attributes :species, :nickname
+    attributes :species, :nickname, :trainer_id
 end
