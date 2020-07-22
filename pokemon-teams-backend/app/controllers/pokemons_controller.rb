04d62df7354e872b808ecd7986d08fe661fc6202
SPerfors <stevecanbuild@gmail.com>
2020-07-22 13:50:08 -0600

    Done.

--- pokemon-teams-backend/app/controllers/pokemons_controller.rb
+++ pokemon-teams-backend/app/controllers/pokemons_controller.rb
@@ -10,4 +10,20 @@ class PokemonsController < ApplicationController
         render json: PokemonSerializer.new(pokemon)
     end
 
+    def new 
+    end
+
+    def create 
+        trainer = Trainer.find_by(id: params['trainer_id'])
+        if trainer.pokemons.count < 6 
+            nickname = Faker::Name.first_name
+            species = Faker::Games::Pokemon.name 
+            pokemon = Pokemon.create(nickname: nickname, species: species, trainer_id: trainer.id)
+            render json: PokemonSerializer.new(pokemon)
+        else 
+            render json: { status: 'error', message: "Can only hold 6 Pokemon"}
+        end
+    end
+
+
 end
