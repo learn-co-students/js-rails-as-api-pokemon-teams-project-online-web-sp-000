26c7d9d6e35a9944c4dce97ac91418a600d5dab4
gabejoseph <gabriel.joseph4@gmail.com>
2020-07-25 14:47:53 -0400

    Done.

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,15 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+const main = document.querySelector('main')
+
+document.addEventListener("DOMContentLoaded", () => loadTrainers())
+
+const loadTrainers = () => {
+    const TRAINERS_URL = `${BASE_URL}/trainers`
+    fetch(TRAINERS_URL)
+    .then(resp => resp.json())
+    .then(json => console.log("Yuppie!"))
+    
+
+}
