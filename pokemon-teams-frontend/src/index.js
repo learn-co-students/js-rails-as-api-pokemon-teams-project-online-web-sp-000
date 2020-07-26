11e34ee58e21d06df9cdcba72dca98cecfd1aaa2
gabejoseph <gabriel.joseph4@gmail.com>
2020-07-26 08:22:30 -0400

    Done.

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -15,6 +15,32 @@ const loadTrainers = () => {
 }
 
 const renderTrainer = (trainerHash) => {
+    const div = document.createElement("div")
+    const p = document.createElement("p")
+    const button = document.createElement("button")
+    const ul = document.createElement("ul")
     
+    div.setAttribute("class", "card")
+    div.setAttribute("data-id", trainerHash.id)
+    p.innerHTML = trainerHash.name
+    button.setAttribute("data-trainer-id", trainerHash.id)
+    button.innerHTML = "Add Pokemon"
+    // attach even listener to button (click)
+
+    div.appendChild(p)
+    div.appendChild(button)
+    div.appendChild(ul)
+
+    main.appendChild(div)
+
+    trainerHash.pokemons.forEach(pokemon => renderPokemon(pokemon))
+
+}
+
+const renderPokemon = (pokemon) => {
+    const ul = document.querySelector(`div[data-id="${pokemon.trainer_id}"]`)
+    const li = document.createElement("li")
+    const button = document.createElement("button")
+
 
 }
