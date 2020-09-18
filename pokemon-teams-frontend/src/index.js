3f55bd18e8f29d5e44c4ce657a717ff92732176b
github username <github email address>
2020-09-17 23:15:15 -0400

    Done.

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,17 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+
+function fetchTrainers() {
+    fetch (TRAINERS_URL)
+    .then(resp => resp.json())
+    .then(json => displayTrainers(json))
+  }
+  
+  function fetchPokemon(url, div){
+    fetch(url)
+    .then(resp => resp.json())
+    .then(json => displayPokemon(json, div))
+  }
+  
+ 
