172bc6545257405db97e88e2010d7a72dfbabe06
ksummerill <kaitlynsummerill@gmail.com>
2020-09-21 17:46:52 -0700

    Done.

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -3,22 +3,26 @@ const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
 
 document.addEventListener('DOMContentLoaded', function() {
-
+  fetchTrainersAndPokemons()
+  addTrainerCards()
 })
 
 // When a user loads the page, they should see all trainers, with their current team of Pokemon.
 function fetchTrainersAndPokemons() {
   fetch("http://localhost:3000/trainers")
     .then(resp => resp.json())
-    .then(json => addTrainerCards(json))
+    .then(console.log)
 }
 
-function addTrainerCards(json) {
-  const trainers = document.getElementById('data-id');
   // left off here - just trying to create cards for each trainer and add their names from above fetch
-  json.message.forEach(image => {
-    let newImg = document.createElement('img');
-    newImg.src = image;
-    imageDiv.appendChild(newImg);
-  })
+function addTrainerCards(trainers) {
+  trainers.forEach(trainer => {
+    let pokemonList = ""
+    trainer.pokemons.forEach(pokemon => {
+      pokemonList += `
+        <li>${pokemon.nickname} (${pokemon.species})
+        <button class="release" data-pokemon-id="${pokemon.id}">Release</button>
+        </li>`
+    })
+})
 }
