e46884ceea5817a1d3b92e20da9dc475c9d2b122
carrollm2 <carrollm2@gmail.com>
2020-10-05 21:56:40 -0400

    Done.

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -6,33 +6,47 @@ const POKEMONS_URL = `${BASE_URL}/pokemons`
 document.addEventListener('DOMContentLoaded', (event) => {
     console.log('DOM fully loaded and parsed');
 
-    const main = document.querySelector('main');    
+    const main = document.querySelector('main');
 
-    const divCard = document.createElement('div');
-    divCard.className = "card";
-    divCard.setAttribute("data-id", "1");
-    main.appendChild(divCard);
+    fetch('http://127.0.0.1:3000/trainers')
+    .then(function(response) {
+        return response.json();
+      })
+      .then(function(trainers){
+        console.log('@@Hello', trainers)
+        for (trainer of trainers){
+            console.log('@@Trainer', trainer.name)
+            const divCard = document.createElement('div');
+            divCard.className = "card";
+            divCard.setAttribute("data-id", trainer.id);
+            main.appendChild(divCard);
 
+            const trainerName = document.createElement('p');
+            divCard.appendChild(trainerName);
+            trainerName.innerText = trainer.name;            
 
-    const trainerName = document.createElement('p')
-    divCard.appendChild(trainerName)
-    trainerName.innerText = "Prince"
+            const addButton = document.createElement('button');
+            addButton.setAttribute("data-trainer-id", trainer.id);
+            addButton.innerText = "Add Pokemon";
+            divCard.appendChild(addButton);
 
+            const unorderedList = document.createElement('ul');
+            for (let i = 0; i < trainer.pokemons.length; i++){
+                const specificPokemon = document.createElement('li')
+                specificPokemon.innerText = trainer.pokemons[i].nickname + ' ' + '(' + trainer.pokemons[i].species + ')'
+                const releaseButton = document.createElement('button')
+                releaseButton.className = 'release'
+                releaseButton.setAttribute("data-pokemon-id", trainer.pokemons[i].id)
+                releaseButton.innerText = "Release"
+                specificPokemon.appendChild(releaseButton)
+                unorderedList.appendChild(specificPokemon)
+            }      
+            
+            divCard.appendChild(unorderedList)
 
-    const addButton = document.createElement('button')
-    addButton.setAttribute("data-trainer-id", "1")
-    addButton.innerText = "Add Pokemon"
-    divCard.appendChild(addButton)
+        }
 
-    const unorderedList = document.createElement('ul')
-    const specificPokemon = document.createElement('li')
-    specificPokemon.innerText = "Jacey (Kakuna)"
-    const releaseButton = document.createElement('button')
-    releaseButton.className = 'release'
-    releaseButton.setAttribute("data-pokemon-id", "140")
-    releaseButton.innerText = "Release"
-    specificPokemon.appendChild(releaseButton)
-    unorderedList.appendChild(specificPokemon)
-    divCard.appendChild(unorderedList)
+      })    
+
 
 });
