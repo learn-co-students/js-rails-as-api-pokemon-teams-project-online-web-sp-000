8bd30127ac446b11d45ce9f944b29140359bb3e0
carrollm2 <carrollm2@gmail.com>
2020-10-10 15:07:29 -0400

    Done.

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -3,122 +3,158 @@ const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
 
 
-document.addEventListener('DOMContentLoaded', (event) => {
-    console.log('DOM fully loaded and parsed');
+const main = document.querySelector("main")
 
-    const main = document.querySelector('main');
+document.addEventListener('DOMContentLoaded', () => loadTrainers)
 
-    fetch('http://127.0.0.1:3000/trainers')
-    .then(function(response) {
-        return response.json();
-      })
-      .then(function(trainers){
-        console.log('@@Hello', trainers)
-        for (trainer of trainers){
-            console.log('@@Trainer', trainer.name)
-            const divCard = document.createElement('div');
-            divCard.className = "card";
-            divCard.setAttribute("data-id", trainer.id);
-            main.appendChild(divCard);
 
-            const trainerName = document.createElement('p');
-            divCard.appendChild(trainerName);
-            trainerName.innerText = trainer.name;            
+const loadTrainers = () => {
 
-            const addButton = document.createElement('button');
-            addButton.setAttribute("data-trainer-id", trainer.id);
-            addButton.innerText = "Add Pokemon";
-            divCard.appendChild(addButton);
+    fetch(TRAINERS_URL)
+        .then(res => res.json())
+        .then(json => {
+            forEach(trainer => renderTrainer(trainer))
+        })
+}
 
-            addButton.addEventListener('click', function(addPokemon){
+const renderTrainer =  (trainerHash) => {   
+    const div = docuement.createElement("div")
+    const p = document.createElement("p")
+    const button = document.createElement("button")
+    const ul = document.createElement("ul")
+
+    div.setAttribute("class", "card")
+    div.setAttribute("data-id", trainerHash.id)
+    p.innerText = trainerHash.named
+    button.setAttribute("data-trainer-id", trainerHash.id)
+    button.innerText = "Add Pokemon"
+    //attach event listiner to button class
+
+    div.appendChild(p)
+    div.appendChild(button)
+    div.appendChild(ul)
+    main.appendChild(div)
+}
+
+
+
+// document.addEventListener('DOMContentLoaded', (event) => {
+//     console.log('DOM fully loaded and parsed');
+
+//     const main = document.querySelector('main');
+
+//     fetch(TRAINERS_URL)
+//     .then(function(response) {
+//         return response.json();
+//       })
+//       .then(function(trainers){
+//         console.log('@@Hello', trainers)
+//         for (trainer of trainers){
+//             console.log('@@Trainer', trainer.name)
+//             const divCard = document.createElement('div');
+//             divCard.className = "card";
+//             divCard.setAttribute("data-id", trainer.id);
+//             main.appendChild(divCard);
+
+//             const trainerName = document.createElement('p');
+//             divCard.appendChild(trainerName);
+//             trainerName.innerText = trainer.name;            
+
+//             const addButton = document.createElement('button');
+//             addButton.setAttribute("data-trainer-id", trainer.id);
+//             addButton.innerText = "Add Pokemon";
+//             divCard.appendChild(addButton);
+
+//             addButton.addEventListener('click', function(addPokemon){
                 
-                addPokemon.preventDefault();
+//                 addPokemon.preventDefault();
 
-                let formData = {
-                    trainer_id: addButton.getAttribute('data-trainer-id')
-                };
+//                 let formData = {
+//                     trainer_id: addButton.getAttribute('data-trainer-id')
+//                 };
                    
 
-                let configObj = {
-                    method: "POST",
-                    headers: {
-                      "Content-Type": "application/json",
-                      "Accept": "application/json"
-                    },
-                    body: JSON.stringify(formData)
-                  };
+//                 let configObj = {
+//                     method: "POST",
+//                     headers: {
+//                       "Content-Type": "application/json",
+//                       "Accept": "application/json"
+//                     },
+//                     body: JSON.stringify(formData)
+//                   };
                    
-                  fetch("http://127.0.0.1:3000/pokemons", configObj)
+//                   fetch(POKEMONS_URL, configObj)
 
-                    .then(function(response) {
-                        console.log('@@Responding...', response);
-                        return response.json();
-                    })
-                    .then(function(object) {
-                        console.log(object);
-                    })
-                    .catch(function(error) {
-                        alert("Sorry, can't successfully add a new Pokemon to team!");
-                        console.log(error.message);
-                    });   
+//                     .then(function(response) {
+//                         console.log('@@Responding...', response);
+//                         return response.json();
+//                     })
+//                     .then(function(object) {
+//                         console.log(object);
+//                     })
+//                     .catch(function(error) {
+//                         alert("Sorry, can't successfully add a new Pokemon to team!");
+//                         console.log(error.message);
+//                     });   
                     
-            });
+//             });
 
 
-            const unorderedList = document.createElement('ul');
-            for (let i = 0; i < trainer.pokemons.length; i++){
-                const specificPokemon = document.createElement('li')
-                specificPokemon.innerText = trainer.pokemons[i].nickname + ' ' + '(' + trainer.pokemons[i].species + ')'
-                const releaseButton = document.createElement('button')
-                releaseButton.className = 'release'
-                releaseButton.setAttribute("data-pokemon-id", trainer.pokemons[i].id)
-                releaseButton.innerText = "Release"
-                specificPokemon.appendChild(releaseButton)
-                unorderedList.appendChild(specificPokemon)
+//             const unorderedList = document.createElement('ul');
+//             for (let i = 0; i < trainer.pokemons.length; i++){
+//                 const specificPokemon = document.createElement('li')
+//                 specificPokemon.innerText = trainer.pokemons[i].nickname + ' ' + '(' + trainer.pokemons[i].species + ')'
+//                 const releaseButton = document.createElement('button')
+//                 releaseButton.className = 'release'
+//                 releaseButton.setAttribute("data-pokemon-id", trainer.pokemons[i].id)
+//                 releaseButton.innerText = "Release"
+//                 specificPokemon.appendChild(releaseButton)
+//                 unorderedList.appendChild(specificPokemon)
 
-                releaseButton.addEventListener('click', function(releasePokemon){
+//                 releaseButton.addEventListener('click', function(releasePokemon){
 
-                    releasePokemon.preventDefault();
+//                     releasePokemon.preventDefault();
 
-                    let formData = {
-                        pokemon_id: releaseButton.getAttribute('data-pokemon-id')
-                    };
+//                     let formData = {
+//                         pokemon_id: releaseButton.getAttribute('data-pokemon-id')
+//                     };
                        
     
-                    let configObj = {
-                        method: "DELETE",
-                        headers: {
-                          "Content-Type": "application/json",
-                          "Accept": "application/json"
-                        },
-                        body: JSON.stringify(formData)
-                      };
+//                     let configObj = {
+//                         method: "DELETE",
+//                         headers: {
+//                           "Content-Type": "application/json",
+//                           "Accept": "application/json"
+//                         },
+//                         body: JSON.stringify(formData)
+//                       };
                        
-                      fetch("http://127.0.0.1:3000/pokemons", configObj)
+//                       fetch(POKEMONS_URL, configObj)
     
-                        .then(function(response) {
-                            console.log('@@Deleting...', response);
-                            return response.json();
-                        })
-                        .then(function(object) {
-                            console.log(object);
-                        })
-                        .catch(function(error) {
-                            alert("Sorry, can't successfully add a new Pokemon to team!");
-                            console.log(error.message);
-                        });   
+//                         .then(function(response) {
+//                             console.log('@@Deleting...', response);
+//                             return response.json();
+//                         })
+//                         .then(function(object) {
+//                             unorderedList.removeChild(specific_pokemon);
+//                             console.log(object);
+//                         })
+//                         .catch(function(error) {
+//                             alert("Sorry, can't successfully add a new Pokemon to team!");
+//                             console.log(error.message);
+//                         });   
                         
-                });                
+//                 });                
                 
 
-            }      
+//             }      
             
-            divCard.appendChild(unorderedList)
+//             divCard.appendChild(unorderedList)
 
-        }
+//         }
 
-      })    
+//       })    
 
-});
+// });
 
 
