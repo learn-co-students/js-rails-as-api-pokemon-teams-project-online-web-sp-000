d2412e101a1f19a5b9dea6c7a52d7ffdb1ef77cb
cantstopcoding <jonathan_koss@yahoo.com>
2020-10-18 13:47:24 -0400

    Done.

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,50 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+
+document.addEventListener("DOMContentLoaded", function() {
+    getTeams()
+});
+
+function getTeams() {
+    fetchData = {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        }
+    }
+    fetch(TRAINERS_URL)
+    .then(function(response) {
+    return response.json();
+    }).then(function(json) {
+    console.log(json);
+    });
+
+}
+
+// let main = document.querySelector('main')
+// let createDiv = document.createElement('div')
+// main.id = 'main'
+// let appendedDiv = document.getElementById("main").appendChild(createDiv);
+// appendedDiv.classList.add("card")
+// appendedDiv.setAttribute("data-id", 1)
+// let p = document.createElement('p')
+// p.innerHTML = "Prince"
+// let addPokemon = document.createElement('button')
+// addPokemon.innerHTML = "Add Pokemon"
+// addPokemon.setAttribute("data-trainer-id", 1)
+// let ul = document.createElement('ul')
+// let li = document.createElement('li')
+// li.innerHTML = "Jacey (Kakuna)"
+// let release = document.createElement('button')
+// release.classList.add("release")
+// release.setAttribute('data-pokemon-id', 1)
+// release.innerHTML = "Release"
+// li.appendChild(release)
+// ul.appendChild(li)
+
+// appendedDiv.appendChild(p)
+// appendedDiv.appendChild(addPokemon)
+// appendedDiv.appendChild(ul)
+
