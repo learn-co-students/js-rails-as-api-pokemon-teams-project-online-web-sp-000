0ad3ab6e523e5dd7120ddcab61d0ea9af495c31d
Jasmine Carter <heyjasminec@gmail.com>
2020-12-07 21:07:31 -0500

    build out front end handling of adding trainers to their own cards

--- pokemon-teams-backend/app/controllers/trainers_controller.rb
+++ pokemon-teams-backend/app/controllers/trainers_controller.rb
@@ -1,2 +1,11 @@
 class TrainersController < ApplicationController
+
+  def index
+    trainers = Trainer.all
+    render json: trainers.to_json(:include => {
+      :pokemons => {:only => [:nickname, :species]}
+      })
+  end
+
+
 end
--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,57 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+
+// for each trainer returned by the trainer endpoint add trainer to card
+
+//should probably do this in response to DOM loading
+
+document.addEventListener("DOMContentLoaded", () => {
+  //add method to feth all trainers
+});
+
+function retrieveTrainers() {
+   fetch("http://localhost:3000/trainers")
+   .then(function(response){
+     return response.json();
+   })
+   .then(function(json){
+     console.log(json)
+      trainers = json
+     addTrainersToCards(trainers)
+   })
+}
+
+//add trainers & their pokeomn to cards for object returned by retrieveTrainers
+function addTrainersToCards(trainers) {
+//assign trainer name as value to this element
+  const allTrainers = trainers
+  for (const trainer of allTrainers) {
+    let card = document.createElement("div");
+    let cardName = document.createElement("p");
+    let name = trainer.name;
+    card.dataset.id = trainer.id;
+    card.className = "card";
+    cardName.innerHTML = name;
+    card.appendChild(cardName);
+    document.querySelector("main").appendChild(card)
+    //create parse pokemon method
+  }
+
+}
+
+//parse pokemon method
+function parseTrainersPokemons(trainer) {
+  const trainerPokemons = trainer.pokemons
+  for (const pokemon of trainerPokemons) {
+    let nickname = pokemon.nickname
+    let species = pokemon.species
+    //for each pokemon, add to trainer card addPokemonToTrainerCard(nickname, species, trainer.id)
+  }
+}
+
+function addPokemonToTrainerCard() {
+  let pokemonList = document.createElement("ul");
+  let li = document.createElement("li");
+}
+//when a user hits add pokemon, and the trainer has space, trainer gets a random new pokeomn?
