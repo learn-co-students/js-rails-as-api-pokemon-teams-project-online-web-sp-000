ebe4c373e770b0e5e795ed680da11c5243f01bae
CoderJay06 <jaycru06@yahoo.com>
2020-12-13 03:17:11 -0500

    Done.

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,14 +1,48 @@
+/*
+    User Stories:
+    
+    - When a user loads the page, they should see all trainers, 
+    with their current team of Pokemon.
+    - Whenever a user hits "Add Pokemon" and they have space on 
+    their team, they should get a new Pokemon.
+    - Whenever a user hits "Release Pokemon" on a specific Pokemon 
+    team, that specific Pokemon should be released from the team.
+*/
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+const main = document.querySelector("main")
 
-/*
-   User Stories:
-   
-   - When a user loads the page, they should see all trainers, 
-   with their current team of Pokemon.
-   - Whenever a user hits "Add Pokemon" and they have space on 
-   their team, they should get a new Pokemon.
-   - Whenever a user hits "Release Pokemon" on a specific Pokemon 
-   team, that specific Pokemon should be released from the team.
- */
+document.addEventListener("DOMContentLoaded", () => getTrainers())
+
+const getTrainers = () => {
+    fetch(TRAINERS_URL)
+        .then(res => res.json())
+        .then(json => {
+            json.forEach(trainer => renderTrainer(trainer))
+        })
+}
+
+const renderTrainer = (trainer) => {
+    const card = document.createElement("div")
+    const nameTag = document.createElement("p")
+    const addButton = document.createElement("button")
+    const pokemonList = document.createElement("ul")
+
+    card.setAttribute("class", "card")
+    card.setAttribute("data-id", trainer.id)
+    nameTag.innerText = trainer.name
+    addButton.setAttribute("data-trainer-id", trainer.id)
+    addButton.innerText = "Add Pokemon"
+        // attatch event listener to button(click)
+
+    trainer.pokemons.forEach(pokemon => {
+        pokemonList.innerHTML += `<li>${pokemon.nickname} (${pokemon.species})
+        <button class="release" data-pokemon-id="${pokemon.id}">Release</button></li>`
+    })
+    card.appendChild(nameTag)
+    card.appendChild(addButton)
+    card.appendChild(pokemonList)
+
+    main.appendChild(card)
+}
