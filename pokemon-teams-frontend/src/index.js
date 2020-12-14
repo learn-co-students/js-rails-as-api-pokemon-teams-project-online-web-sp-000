8b4d7beb1b82dc08a37905ebc6840d71b58903c7
CoderJay06 <jaycru06@yahoo.com>
2020-12-14 04:44:37 -0500

    Done.

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,13 +1,3 @@
-/*
-    User Stories:
-    
-    - When a user loads the page, they should see all trainers, 
-    with their current team of Pokemon.
-    - Whenever a user hits "Add Pokemon" and they have space on 
-    their team, they should get a new Pokemon.
-    - Whenever a user hits "Release Pokemon" on a specific Pokemon 
-    team, that specific Pokemon should be released from the team.
-*/
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
@@ -34,7 +24,6 @@ const renderTrainer = (trainer) => {
     nameTag.innerText = trainer.name
     addButton.setAttribute("data-trainer-id", trainer.id)
     addButton.innerText = "Add Pokemon"
-        // attatch event listener to button(click)
     addButton.addEventListener("click", createPokemon)
     trainer.pokemons.forEach(pokemon => pokemonList.appendChild(renderPokemons(pokemon)))
     card.appendChild(nameTag)
@@ -44,10 +33,8 @@ const renderTrainer = (trainer) => {
 }
 
 const renderPokemons = (pokemon) => {
-    const div = document.querySelector(`div[data-id="${pokemon.trainer_id}"]`)
     const li = document.createElement("li")
     const button = document.createElement("button")
-
     li.innerText = `${pokemon.nickname} (${pokemon.species})`
     button.setAttribute("class", "release")
     button.setAttribute("data-pokemon-id", pokemon.id)
@@ -80,55 +67,31 @@ const createPokemon = (event) => {
                 addPokemon(pokeData)
             }
         })
-        // .catch(error => {
-        //     alert(error.message)
-        // })
 }
 
-const addPokemon = (pokeData) => {
-    const addButton = document.querySelector(`button[data-trainer-id="${pokeData.trainer.id}"]`)
-    const pokemonList = addButton.nextSibling
+const addPokemon = (pokemon) => {
+    const div = document.querySelector(`div[data-id="${pokemon.trainer.id}"]`)
+    const ul = div.children[2]
     const li = document.createElement("li")
     const button = document.createElement("button")
-
-    li.innerText = `${pokeData.nickname} (${pokeData.species})`
+    li.innerText = `${pokemon.nickname} (${pokemon.species})`
     button.setAttribute("class", "release")
-    button.setAttribute("data-pokemon-id", pokeData.id)
+    button.setAttribute("data-pokemon-id", pokemon.id)
     button.innerText = "Release"
+    button.addEventListener("click", releasePokemon)
     li.appendChild(button)
-    pokemonList.appendChild(li)
+    ul.appendChild(li)
 }
 
 const releasePokemon = (event) => {
     event.preventDefault()
-        /*
-        #=> Example Request
-            DELETE /pokemons/:pokemon_id
-            
-            #=> Example Response
-            {"id":147,
-            "nickname":"Gunnar",
-            "species":"Weepinbell",
-            "trainer_id":1}
-         */
     const configObj = {
-            method: "DELETE",
-            headers: {
-                "Content-Type": "application/json",
-                "Accept": "application/json"
-            }
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
         }
-        // let pokemonsUrl = POKEMONS_URL.concat(`/${event.target.dataset.pokemonId}`)
+    }
     fetch(`${POKEMONS_URL}/${event.target.dataset.pokemonId}`, configObj)
     event.target.parentElement.remove()
-        // .then(pokeData => {
-        //     debugger
-
-    //     const releaseButton = document.querySelector(`
-    // button[data - pokemon - id = "${pokeData.id}"]
-    //     `)
-    // //     const li = releaseButton.parentElement
-    // //     const ul = li.parentElement
-    // //     ul.removeChild(li)
-    // // })
 }
