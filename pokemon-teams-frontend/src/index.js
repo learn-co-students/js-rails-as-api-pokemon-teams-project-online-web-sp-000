7bba7f7935748eb4debe48e781ed45b0bc2de471
ashleymader <ashley.mader12@gmail.com>
2021-01-07 13:11:35 -0500

    Done.

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,36 +1,53 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+const pokemonTeamsContainer = document.querySelector('main')
+const trainerCards = document.querySelectorAll('.card')
 
 
 document.addEventListener("DOMContentLoaded", () => {
     console.log("dom has loaded");
     getTrainers();
-  });
+});
 
-  function getTrainers() {
+function getTrainers(){
     fetch(TRAINERS_URL)
     .then(response => response.json())
-    .then(json => renderTeams(json));
-  }
+    .then(json => {
+        json.forEach(trainer => renderTrainer(trainer))});
+}   
 
-  function renderTeams(json) {
-      const pokemonTeamsContainer = document.querySelector('main')
-      let trainerAndTeam = json.map(function(json) {
-          return `
-        <div class="card" data-id=${json.id}><p>${json.name}</p>
-            <button data-trainer-id=${json.id}>Add Pokemon</button>
-            <ul>
-                <li>${json.pokemons[2]}<button class="release" data-pokemon-id="140">Release</button></li>
-                <li>Zachariah (Ditto) <button class="release" data-pokemon-id="141">Release</button></li>
-                <li>Mittie (Farfetch'd) <button class="release" data-pokemon-id="149">Release</button></li>
-                <li>Rosetta (Eevee) <button class="release" data-pokemon-id="150">Release</button></li>
-                <li>Rod (Beedrill) <button class="release" data-pokemon-id="151">Release</button></li>
-            </ul>
-        </div>
-        `
-      })
-          console.log(trainerAndTeam)
-      }
+function renderTrainer(trainer) {
+    const div = document.createElement('div')
+    const p = document.createElement('p')
+    const addButton = document.createElement('button')
+    const ul = document.createElement('ul')
 
-  
+    div.setAttribute('class', 'card')
+    div.setAttribute('data-id', trainer.id)
+    p.innerHTML = trainer.name
+    addButton.setAttribute('data-trainer-id', trainer.id)
+    addButton.innerHTML = 'Add Pokemon'
+
+    div.appendChild(p)
+    div.appendChild(addButton)
+    div.appendChild(ul)
+
+    pokemonTeamsContainer.appendChild(div)
+    trainer.pokemons.forEach(pokemon => renderPokemon(pokemon))
+}
+
+function renderPokemon(pokemon) {
+    const ul = document.querySelector(`div[data-id='${pokemon.trainer_id}']`)
+    const li = document.createElement('li')
+    const releaseButton = document.createElement('button')
+
+    li.innerHTML = `${pokemon.nickname} (${pokemon.species})`
+    releaseButton.setAttribute('class', 'release')
+    releaseButton.setAttribute('data-pokemon-id', pokemon.id)
+    releaseButton.innerHTML = 'Release'
+
+    li.appendChild(releaseButton)
+    ul.appendChild(li)
+
+}
