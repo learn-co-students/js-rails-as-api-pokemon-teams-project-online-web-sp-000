fe2eeb438756e036944b96a456516b7804cb5082
Jessica Natovich <jessicanatovich@yahoo.com>
2021-01-30 04:57:50 +0000

    Done.

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,34 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+
+
+#=> Example Request
+GET /trainers
+ 
+#=> Example Response
+[
+  {
+    "id":1,
+    "name":"Prince",
+    "pokemons":[
+      {
+        "id":140,
+        "nickname":"Jacey",
+        "species":"Kakuna",
+        "trainer_id":1
+      },
+      {
+        "id":141,
+        "nickname":"Zachariah",
+        "species":"Ditto",
+        "trainer_id":1
+      },
+      // ...
+    ]
+  }
+  // ..
+
+
+
+
