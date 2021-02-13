7cb17a4153b267d3a9e7c7fb13912e6166d38428
jrog20 <jaimerogstad@gmail.com>
2021-02-13 12:04:29 -0500

    Done.

--- pokemon-teams-backend/app/controllers/pokemons_controller.rb
+++ pokemon-teams-backend/app/controllers/pokemons_controller.rb
@@ -1,2 +1,26 @@
 class PokemonsController < ApplicationController
+  def index
+    pokemons = Pokemon.all
+    render json: pokemons
+  end
+
+  def show
+    pokemon = Pokemon.find_by(id: params[:id])
+    render json: pokemon
+  end
+
+  def create
+    trainer = Trainer.find_by(id: params[:trainer_id])
+    if trainer.pokemons.count < 6
+      pokemon = trainer.pokemons.build({
+        nickname = Faker::Name.first_name
+        species = Faker::Games::Pokemon.name
+      })
+      render json: pokemon
+  end
+
+  def destroy
+    pokemon = Pokemon.find_by(id: params[:id])
+    pokemon.destroy
+  end
 end
--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -2,3 +2,10 @@ const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
 
+document.addEventListener('DOMContentLoaded', () => {
+  
+})
+
+function releasePokemon() {
+  
+}
