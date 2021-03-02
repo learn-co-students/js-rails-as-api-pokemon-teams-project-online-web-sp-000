6293505dd374439b540bcfe5eab9c319f7845775
Chandra Lama <iamme24cl@gmail.com>
2021-03-02 09:40:49 -0800

    Done.

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,18 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+
+
+function getTrainers() {
+  return fetch(BASE_URL)
+    .then(response => response.json())
+    .then(trainers => {
+      trainers.forEach(trainer => {
+        displayTrainers(trainer);
+      })
+    });
+}
+
+function displayTrainers(trainer) {
+  
+}
