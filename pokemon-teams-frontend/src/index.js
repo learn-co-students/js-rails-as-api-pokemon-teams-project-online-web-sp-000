829420040fb8d8917da4b7322dd639c73a80138d
Chandra Lama <iamme24cl@gmail.com>
2021-03-02 18:33:11 -0800

    implemented displaying all trainers and pokemons

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,6 +1,7 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+const main = document.querySelector('main');
 
 
 function getTrainers() {
@@ -9,13 +10,38 @@ function getTrainers() {
     .then(trainers => {
       trainers.forEach(trainer => {
        console.log(trainer);
-       renderTeams();
+       renderTeams(trainer);
       })
     });
 }
 
-function renderTeams() {
+function newPokemon(pokemon) {
+  const li = document.createElement('li');
+  li.textContent = `${pokemon.nickname} (${pokemon.species})`;
+  return li;
+}
+
+
+function renderTeams(trainer) {
+  const divCard = document.createElement('div');
+  const p = document.createElement('p');
+  const addPokemonBtn = document.createElement('button');
+  const pokemonsList = document.createElement('ul');
+  const pokemons = trainer.pokemons;
   
+  p.innerText = trainer.name;
+  
+  addPokemonBtn.textContent = "Add Pokemon";
+  addPokemonBtn.setAttribute('data-trainer-id', trainer.id)
+  
+  pokemons.forEach(pokemon => {
+    pokemonsList.appendChild( newPokemon(pokemon) );
+  })
+  
+  divCard.className = "card";
+  divCard.setAttribute('data-id', trainer.id);
+  divCard.append(p, addPokemonBtn, pokemonsList);
+  main.appendChild(divCard);
 }
 
 getTrainers();
