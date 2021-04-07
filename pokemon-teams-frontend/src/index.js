550a12f151097f39b4b2233401863819e00fb5bf
Jenise Ruehle <jeniseruehle@gmail.com>
2021-04-07 10:13:40 -0500

    Done.

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,12 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+const mainDiv = document.querySelector('main')
+
+
+
+
+fetch(TRAINERS_URL)
+  .then(res => res.json())
+  .then(putTrainersOnPage)
+  
