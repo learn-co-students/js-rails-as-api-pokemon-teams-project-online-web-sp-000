2ee394d0c063e3e1f1fc0bf0f410e81bbcfc176b
Anna Guerrero <acg2176@columbia.edu>
2021-04-21 17:27:29 -0400

    Done.

---
+++ pokemon-teams-backend/app/controllers/pokemons_controller.rb
@@ -0,0 +1,17 @@
+class PokemonsController < ApplicationController
+    before_action :find_pokemon, only: [:edit, :update, :destroy]
+
+	def index
+		pokemons = Pokemon.all
+		render json: PokemonSerializer.new(pokemons)
+	end
+
+	def show
+		pokemon = Pokemon.find_by(id: params[:id])
+		render json: PokemonSerializer.new(pokemon)
+	end
+
+	def find_pokemon
+		@pokemon = Pokemon.find_by(id: params[:id])
+	end
+end
