930e59901ccaaee714d8110bbd6bf9f3e435340a
deliaconstantino <dconstan92@gmail.com>
2021-04-26 18:24:25 -0400

    Done.

--- pokemon-teams-backend/app/controllers/pokemons_controller.rb
+++ pokemon-teams-backend/app/controllers/pokemons_controller.rb
@@ -1,18 +1,6 @@
 class PokemonsController < ApplicationController
   def index
     pokemons = Pokemon.all
-    # teams = {}
-    # pokemons.each do |poke|
-    #   if teams.has_key?(teams[poke.trainer_id])
-    #     teams[poke.trainer_id] << poke
-    #   else
-    #     teams[poke.trainer_id] = []
-    #     teams[poke.trainer_id] << [poke]
-    #   end
-
-    # end
-    # render json: teams
-
     render json: pokemons
   end
 
@@ -20,7 +8,12 @@ class PokemonsController < ApplicationController
     name = Faker::Name.first_name
     species = Faker::Games::Pokemon.name
     id = params["trainer_id"]
-    Pokemon.create(nickname: name, species: species, trainer_id: id)
+    pokemon = Pokemon.create(nickname: name, species: species, trainer_id: id)
+    render json: pokemon
   end
 
+  def destroy
+    pokemon = Pokemon.find_by(id: params[:id])
+    pokemon.destroy
+  end
 end
--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -4,116 +4,115 @@ const POKEMONS_URL = `${BASE_URL}/pokemons`
 
 const main = document.getElementsByTagName("main")[0];
 
-fetch(TRAINERS_URL)
-  .then(resp => resp.json())
-  .then(json => {
-    json.forEach( element => {
-      createDivCard(element);
+//Load Pokemons
+document.addEventListener('DOMContentLoaded', () => {
+  fetch(TRAINERS_URL)
+    .then(resp => resp.json())
+    .then(json => {
+      json.forEach( element => {
+        createDivCard(element);
+      })
     })
+})
+
+function createDivCard(element) {
+  const div = document.createElement("div");
+  div.classList.add("card");
+  div.setAttribute("data-id", element.id)
+  main.appendChild(div);
+  createPTagWithName(element, div);
+  createAddPokemonButton(element, div);
+  createListOfPokemon(element, div);
+}
+
+function createPTagWithName(element, div) {
+  const pTag = document.createElement("p");
+  pTag.innerText = element.name
+  div.appendChild(pTag);
+}
+
+function createAddPokemonButton(element, div) {
+  const button = document.createElement("button");
+  button.setAttribute("data-trainer-id", element.id)
+  button.innerHTML = "Add Pokemon"
+  button.addEventListener("click", addPokemon);
+  div.appendChild(button);
+}
+
+function createListOfPokemon(element, div) {
+  const ul = document.createElement("ul");
+  div.appendChild(ul);
+  element.pokemons.forEach(poke => {
+    createListItem(poke, ul);
   })
+}
 
-  function createDivCard(element) {
-    const div = document.createElement("div");
-    div.classList.add("card");
-    div.setAttribute("data-id", element.id)
-    main.appendChild(div);
+function createListItem(poke, ul) {
+  const li = document.createElement("li");
+  ul.appendChild(li);
+  li.innerHTML = `${poke.nickname} (${poke.species})`
+  createReleaseButton(poke, li);
+}
 
-    createPTagWithName(element, div);
+function createReleaseButton(poke, li) {
+  const button = document.createElement("button");
+  button.classList.add("release");
+  button.setAttribute("data-pokemon-id", poke.id)
+  button.innerHTML = "Release"
+  button.addEventListener("click", releasePokemon);
+  li.appendChild(button);
+}
 
-    createAddPokemonButton(element, div);
+//addPokemon function
+function addPokemon(event) {
+  const trainerId = event.target.dataset.trainerId;
+  const div = document.querySelector(`[data-id='${trainerId}']`)
+  const lis = div.querySelectorAll("li");
 
-    createListOfPokemon(element, div);
-  }
+  if (lis.length < 6) {
 
-  function createPTagWithName(element, div) {
-    const pTag = document.createElement("p");
-    pTag.innerText = element.name
-    div.appendChild(pTag);
-  }
-
-  function createAddPokemonButton(element, div) {
-    const button = document.createElement("button");
-    button.setAttribute("data-trainer-id", element.id)
-    button.innerHTML = "Add Pokemon"
-    button.addEventListener("click", function(){ addPokemon(element.id) })
-    div.appendChild(button);
-  }
-
-  function createListOfPokemon(element, div) {
-    const ul = document.createElement("ul");
-    div.appendChild(ul);
-
-    element.pokemons.forEach(poke => {
-      createListItem(poke, ul);
-    })
-  }
-
-  function createListItem(poke, ul) {
-    const li = document.createElement("li");
-    ul.appendChild(li);
-    li.innerHTML = `${poke.nickname} (${poke.species})`
-
-    createReleaseButton(poke, li);
-  }
-
-  function createReleaseButton(poke, li) {
-    const button = document.createElement("button");
-    button.classList.add("release");
-    button.setAttribute("data-pokemon-id", poke.id)
-    button.innerHTML = "Release"
-    li.appendChild(button);
-  }
-
-  //Above Page Load; below other functions
-
-  function addPokemon(trainerId) {
-    const div = document.querySelector(`[data-id='${trainerId}']`)
-    const lis = div.querySelectorAll("li");
-
-    if (lis.length < 6) {
-      console.log("do something");
-      //implement fetch with post
-      const configObj = {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          "Accept": "application/json"
-        },
-        body: JSON.stringify({
-          trainer_id: trainerId
-        })
-      }
-
-      fetch(POKEMONS_URL, configObj)
-        .then(resp => resp.json())
-        .then(json => {
-          console.log(json)
-        })
-    } else {
-      console.log("do nothing");
+    const configObj = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify({
+        trainer_id: trainerId
+      })
     }
 
-    // JSON.stringify({
-    //   dogName: "Byron",
-    //   dogBreed: "Poodle"
-    // })
+    fetch(POKEMONS_URL, configObj)
+      .then(resp => resp.json())
+      .then(json => {
+        const ul = div.querySelector('ul')
+        createListItem(json, ul);
+      })
+  } else {
+    console.log("do nothing");
+  }
+}
 
+// Release pokemon function
 
-
-
-
-    //first check number of pokemong under this trainer, if less than 6 do:
-    //post request to localhost/pokemons (using fetch() with a config hash to do non-default post request); pass in trainer_id in the body
-    //in the pokemon create action, set
-    // name = Faker::Name.first_name
-    // species = Faker::Games::Pokemon.name
-    // Pokemon.create(nickname: name, species: species, trainer_id: trainer.id)
-    //and pass id to trainer.id
-    //else: console log (not enough space) and be done
-
+function releasePokemon(event) {
+  const pokemonId = event.target.dataset.pokemonId
+  const deleteObj = {
+    method: "DELETE",
+    headers: {
+      // "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: {
+      pokemon_id: pokemonId
+    }
   }
 
+  fetch(`${POKEMONS_URL}/${pokemonId}`, deleteObj)
+    .then(() => {
+      const button = document.querySelector(`[data-pokemon-id='${pokemonId}']`)
+      const li = button.parentElement;
+      li.remove();
+    })
 
-  // fetch(POKEMONS_URL)
-  // .then(resp => resp.json())
-  // .then(json => console.log(json));
+}
