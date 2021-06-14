ddec13e8677ecf6b250d63b97636dc127ac679ca
McCray <tubach75@hotmail.com>
2021-06-14 12:05:15 -0400

    Done.

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,18 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+
+
+fetch(POKEMONS_URL)
+.then(response => response.json())
+.then(object => object.forEach(poke => {
+    const p = document.createElement('p');
+    const m = document.querySelector('main');
+    p.innerText = poke.nickname; 
+    m.appendChild(p);
+})
+)
+
+fetch(TRAINERS_URL)
+.then(response => response.json())
+.then(object => object.forEach(trainer => console.log(trainer.name)));
